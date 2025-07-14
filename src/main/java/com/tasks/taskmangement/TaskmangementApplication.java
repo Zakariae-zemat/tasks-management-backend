3fa978d821ad9e8184d1bd4d57b15f4137a1f3a1
zakariae-zemat <zakariaezemat@gmail.com>
2025-07-14 17:32:20 +0100

    disable metrics

--- src/main/java/com/tasks/taskmangement/TaskmangementApplication.java
+++ src/main/java/com/tasks/taskmangement/TaskmangementApplication.java
@@ -3,7 +3,11 @@ package com.tasks.taskmangement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration.class
+    }
+)
 public class TaskmangementApplication {
 
 	public static void main(String[] args) {
